c710bde53a007dcc38d6280f95802445d4e2f12f
Mutesasira Moses <mozzymutesa@gmail.com>
2023-03-02 22:59:10 +0000

    select the first patient record incase duplicate records already exist

--- api/src/main/java/org/openmrs/module/xdsbrepository/impl/XDSbServiceImpl.java
+++ api/src/main/java/org/openmrs/module/xdsbrepository/impl/XDSbServiceImpl.java
@@ -970,10 +970,12 @@ public class XDSbServiceImpl extends BaseOpenmrsService implements XDSbService {
 		}
 
 		List<Patient> patients = ps.getPatients(null, id.getIdentifier(), Collections.singletonList(idType), true);
-		if (patients.size() > 1) {
+		
+		/*if (patients.size() > 1) {
 			throw new PatientIdentifierException("Multiple patients found for this identifier: " + id.getIdentifier()
 			        + ", with id type: " + id.getAssigningAuthority().getAssigningAuthorityId());
-		}
+		}*/
+
 		// try to search and  match patients using other patient identifiers
 		if (patients.isEmpty()) {
 			Map<String, SlotType1> slots = InfosetUtil.getSlotsFromRegistryObject(eo);
@@ -988,12 +990,7 @@ public class XDSbServiceImpl extends BaseOpenmrsService implements XDSbService {
 					patients = ps.getPatients(null, identifier.getIdentifier(), Collections.singletonList(identifierType),
 					    true);
 					
-					if (patients.size() > 1) {
-						throw new PatientIdentifierException(
-						        "Multiple patients found for this identifier: " + identifier.getIdentifier()
-						                + ", with id type: " + identifier.getAssigningAuthority().getAssigningAuthorityId());
-					}
-					if (patients.size() == 1) {
+					if (!patients.isEmpty()) {
 						break;
 					}
 				}
